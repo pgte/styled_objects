3dc24668c42aacdbbd43a7089302bacde7b8f0ce
Pedro Teixeira <pedro.teixeira@gmail.com>
2009-10-14 00:09:53 +0100

    CSS file names are sorted before beeing put in URL

--- lib/styled_objects/helpers/stylesheet_link_tag.rb
+++ lib/styled_objects/helpers/stylesheet_link_tag.rb
@@ -8,7 +8,7 @@ module StyledObjects
       
       module InstanceMethods
         def so_stylesheet_link_tag
-          return "<link href=\"#{styledobject_path styled_objects_partial_css_paths.collect {|path| URI.encode(path.gsub(/:|\.css/, ''))}.join(':')}?#{styled_objects_max_mtime}\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />"
+          return "<link href=\"#{styledobject_path styled_objects_partial_css_paths.sort.collect {|path| URI.encode(path.gsub(/:|\.css/, ''))}.join(':')}?#{styled_objects_max_mtime}\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />"
         end
       end
       
