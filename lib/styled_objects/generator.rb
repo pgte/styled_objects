2501bf1e872ff7716006a3105adfe3c47bdf13d0
Pedro Teixeira <pedro.teixeira@gmail.com>
2009-10-13 12:05:06 +0100

    q

--- lib/styled_objects/generator.rb
+++ lib/styled_objects/generator.rb
@@ -6,10 +6,17 @@ class StyledObjects::Generator
       file_path = self._find_partial_stylesheet(view_paths, "#{path}.css")
       if file_path
         File.open(file_path) do |file|
-          ".#{StyledObjects::ClassNameBuilder.build_class_name_from_partial_path(path)}{#{file.read}}"
+          ".#{StyledObjects::ClassNameBuilder.build_class_name_from_partial_path(path)}{\n#{file.read}\n}\n"
         end
       end
     end.compact.join("\n")
+    file_path = self._find_partial_stylesheet(view_paths, "/globals.css")
+    if file_path
+      File.open(file_path) do |file|
+        content = "#{file.read}\n#{content}"
+      end
+    end
+    
     ::Less::Engine.new(content).to_css
   end
   
